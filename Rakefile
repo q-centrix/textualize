593f6c332fae994869e782663ea9298bc8215e00
Nicholas Shook <nshook@q-centrix.com>
2015-04-08 16:19:23 -0700

    fixed specs

--- Rakefile
+++ Rakefile
@@ -1,2 +1,11 @@
-require "bundler/gem_tasks"
+require 'bundler/gem_tasks'
+require 'rubygems'
 
+# require 'cucumber'
+# require 'cucumber/rake/task'
+
+# task default: %i(features)
+
+# Cucumber::Rake::Task.new(:features) do |t|
+#   t.cucumber_opts = "features --format pretty"
+# end
--- lib/textualize.rb
+++ lib/textualize.rb
@@ -1,35 +1,36 @@
-require "textualize/version"
+require 'thor'
+Dir["#{__dir__}/**/*.rb"].each { |f| require f }
 
 module Textualize
   class Textualize < Thor
     register(New, 'new', 'new', 'create a new documentation project')
 
-    register(
-      Server,
-      'server',
-      'server',
-      'create a folder structure of json responses'
-    )
-
-    register(
-      RequestSpecs,
-      'request_specs',
-      'request_specs',
-      'create request specs from your raml files'
-    )
-
-    register(
-      Seeds,
-      'seeds',
-      'seeds',
-      'create a db:seed from your raml files'
-    )
-
-    register(
-      Factories,
-      'factories',
-      'factories',
-      'create angular factories from your raml files'
-    )
+    # register(
+    #   Server,
+    #   'server',
+    #   'server',
+    #   'create a folder structure of json responses'
+    # )
+    #
+    # register(
+    #   RequestSpecs,
+    #   'request_specs',
+    #   'request_specs',
+    #   'create request specs from your raml files'
+    # )
+    #
+    # register(
+    #   Seeds,
+    #   'seeds',
+    #   'seeds',
+    #   'create a db:seed from your raml files'
+    # )
+    #
+    # register(
+    #   Factories,
+    #   'factories',
+    #   'factories',
+    #   'create angular factories from your raml files'
+    # )
   end
 end
