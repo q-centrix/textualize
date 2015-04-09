a73af9406408c0be41cd52047887e53101653e31
Nicholas Shook <nshook@q-centrix.com>
2015-04-08 19:27:39 -0700

    fixed spec_helper

---
+++ lib/textualize/transformers/two_hundred_four.rb
@@ -0,0 +1,18 @@
+
+module Textualize
+  class TwoHundredFour
+
+    pattr_initialize :response
+
+    def transform
+      transformed_hash = {}
+
+      transformed_hash[:response_code] = response.keys.first.to_s.to_i
+
+      transformed_hash[:body] = ''
+
+      return transformed_hash
+    end
+
+  end
+end
