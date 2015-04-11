5be89c8039768f6656ed63456d0522a25adddc53
Nicholas Shook <nshook@q-centrix.com>
2015-04-11 13:58:14 -0700

    updated readme

--- lib/textualize/tasks/helpers/route_hash_creator.rb
+++ lib/textualize/tasks/helpers/route_hash_creator.rb
@@ -4,7 +4,7 @@ module Textualize
     pattr_initialize :json_from_gulp_task
 
     def create_route_hashes
-      json_from_gulp_task.fetch(:'resources').flat_map do |resource|
+      json_from_gulp_task.fetch(:resources).flat_map do |resource|
         create_method_hashes(resource)
       end
     end
@@ -27,7 +27,7 @@ module Textualize
                         resource.fetch(:relativeUri)
                       end
 
-      method_hashes = method_hashes + resource.fetch(:'methods').map do |method|
+      method_hashes = method_hashes + resource.fetch(:methods).map do |method|
         method_hash = {}
         method_hash[:verb] = method.fetch(:method)
         method_hash[:url] = base_path + relative_path
@@ -42,7 +42,7 @@ module Textualize
     end
 
     def base_path
-      URI(json_from_gulp_task.fetch(:'baseUri')).path
+      URI(json_from_gulp_task.fetch(:baseUri)).path
     end
   end
 end
