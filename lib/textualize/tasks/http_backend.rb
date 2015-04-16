6d642d2921cbaedd7b057add2e13fa95e076f629
Nicholas Shook <nshook@q-centrix.com>
2015-04-16 09:36:17 -0700

    updated to @samnang's comments

--- lib/textualize/tasks/http_backend.rb
+++ lib/textualize/tasks/http_backend.rb
@@ -11,8 +11,6 @@ module Textualize
     )
 
     def create_angular_module
-      delete_dist_file_if_exsts
-
       create_basic_http_backend_file
 
       append_uglified_hashes
@@ -20,19 +18,11 @@ module Textualize
 
     private
 
-    def make_directory_if_it_does_not_exist(directory)
-      FileUtils.mkdir_p(directory) unless File.directory? directory
-    end
-
-    def delete_dist_file_if_exsts
-      FileUtils.rm dist_file if File.exists? dist_file
-    end
-
     def create_basic_http_backend_file
-      File.open(dist_file, 'a') do |file|
+      File.open(dist_file, 'w') do |file|
         file.write(
           Uglifier.compile(
-            Pathname.new("#{template_directory}module.js").read
+            File.read("#{template_directory}/module.js")
           )
         )
       end
@@ -44,13 +34,19 @@ module Textualize
       'angular/textualize_http_backend.min.js'
     end
 
+    def make_directory_if_it_does_not_exist(directory)
+      FileUtils.mkdir_p(directory) unless File.directory? directory
+    end
+
     def http_backend_template(route_hash)
-      pathname = Pathname.new("#{template_directory}#{route_hash.verb}.js.erb")
-      ERB.new(pathname.read).result(route_hash.instance_eval { binding })
+      ERB.new(
+        File.read("#{template_directory}/#{route_hash.verb}.js.erb")
+      ).result(route_hash.instance_eval { binding })
     end
 
     def template_directory
-      __dir__.chomp('tasks') + 'templates/http_backend/'
+      path = File.join(__dir__, '..', 'templates/http_backend')
+      File.expand_path(path)
     end
 
     def append_uglified_hashes
