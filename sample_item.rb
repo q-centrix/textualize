035b18ce87b2c056e41c1b9406cdd9dc18779f49
Nicholas Shook <nshook@q-centrix.com>
2015-04-30 13:15:00 -0700

    style sample_item

--- sample_item.rb
+++ sample_item.rb
@@ -1,41 +1,41 @@
 {
-  verb: "get",
-  url: "/api/v1/items",
-  relative_path: "/items",
-  type: "collection-ro",
-  secured_by: ["staff"],
-  name: "items",
+  verb: 'get',
+  url: '/api/v1/items',
+  relative_path: '/items',
+  type: 'collection-ro',
+  secured_by: ['staff'],
+  name: 'items',
   response_code: 200,
   body:  {
     data:  [
       {
-        id: "1",
-        type: "item",
-        created_at: "1430407103",
-        updated_at: "1430407180",
-        name: "Hamburger",
+        id: '1',
+        type: 'item',
+        created_at: '1430407103',
+        updated_at: '1430407180',
+        name: 'Hamburger',
         links: {
-          self: "https://app.my-awesome-restaurant.com/api/v1/items/1"
+          self: 'https://app.my-awesome-restaurant.com/api/v1/items/1'
         }
       },
       {
-        id: "2",
-        type: "item",
-        created_at: "1430407103",
-        updated_at: "1430407180",
-        name: "Cheese burger",
+        id: '2',
+        type: 'item',
+        created_at: '1430407103',
+        updated_at: '1430407180',
+        name: 'Cheese burger',
         links: {
-          self: "https://app.my-awesome-restaurant.com/api/v1/items/2"
+          self: 'https://app.my-awesome-restaurant.com/api/v1/items/2'
         }
       },
       {
-        id: "3",
-        type: "item",
-        created_at: "1430407103",
-        updated_at: "1430407180",
-        name: "French Fries",
+        id: '3',
+        type: 'item',
+        created_at: '1430407103',
+        updated_at: '1430407180',
+        name: 'French Fries',
         links: {
-          self: "https://app.my-awesome-restaurant.com/api/v1/items/3"
+          self: 'https://app.my-awesome-restaurant.com/api/v1/items/3'
         }
       }
     ]
