b9741e16bb0319ca095e53e14603c4c8d828da89
Nicholas Shook <nshook@q-centrix.com>
2015-05-26 16:46:15 -0700

    scoped it

--- lib/textualize/tasks/http_backend.rb
+++ lib/textualize/tasks/http_backend.rb
@@ -8,32 +8,34 @@ module Textualize
       'Creates an angular 1.x module based off ngMockE2E that stubs a backend'
     )
 
-    def create_angular_module
-      create_basic_http_backend_file
+    def create_backend_angular_modules
+      RouteHashes.filenames_and_hashes.each do |(filename, hashes)|
+        puts hashes
+        puts filename
 
-      append_uglified_hashes
+        FileUtils.mkdir_p("dist/#{filename}/angular/backend")
+
+        File.open(dist_file(filename), 'w') do |file|
+          file.write(
+            File.read("#{template_directory}/module.js")
+          )
+        end
+
+        hashes.each do |route_hash|
+          modified_hash = replace_sample_id_with_1(route_hash)
+          File.open(dist_file(filename), 'a') do |file|
+            file.write(
+              http_backend_template(modified_hash)
+            )
+          end
+        end
+      end
     end
 
     private
 
-    def create_basic_http_backend_file
-      File.open(dist_file, 'w') do |file|
-        file.write(
-          File.read("#{template_directory}/module.js")
-        )
-      end
-    end
-
-    def dist_file
-      FileUtils.mkdir_p('dist/angular/backend')
-
-      'dist/angular/backend/fake_http_backend.min.js'
-    end
-
-    def http_backend_template(route_hash)
-      ERB.new(
-        File.read("#{template_directory}/#{route_hash.verb}.js.erb")
-      ).result(route_hash.instance_eval { binding })
+    def dist_file(filename)
+      "dist/#{filename}/angular/backend/fake_http_backend.min.js"
     end
 
     def template_directory
@@ -41,19 +43,16 @@ module Textualize
       File.expand_path(path)
     end
 
-    def append_uglified_hashes
-      RouteHashes.hashes.each do |route_hash|
-        # replace sample ids with 1
-        modified_hash = route_hash
-        modified_hash.relative_path.gsub!(/{.*}/, '1')
-        modified_hash.url.gsub!(/{.*}/, '1')
+    def replace_sample_id_with_1(route_hash)
+      route_hash.relative_path.gsub!(/{.*}/, '1')
+      route_hash.url.gsub!(/{.*}/, '1')
+      return route_hash
+    end
 
-        File.open(dist_file, 'a') do |file|
-          file.write(
-            http_backend_template(modified_hash)
-          )
-        end
-      end
+    def http_backend_template(route_hash)
+      ERB.new(
+        File.read("#{template_directory}/#{route_hash.verb}.js.erb")
+      ).result(route_hash.instance_eval { binding })
     end
   end
 end
